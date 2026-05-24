d0a2ab3d5089fdd08201bb5c3c478f318d0909cf
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-05-24 15:53:32 +0930

    Create socket

--- main.c
+++ main.c
@@ -14,10 +14,31 @@ int main(int argc, char *argv[]) {
   const char *message = argv[3];
 
   struct sockaddr_in peer_adr = {
-      .sin_family =
-          AF_INET, // AF_INET = IPv4 address family, can use AF_INET6 for IPv6
-      .sin_port = htons(peer_port) // htons = Host to Network Short
+      .sin_family = AF_INET, // Socket Internet Family AF_INET = IPv4 address
+                             // family, can use AF_INET6 for IPv6
+      .sin_port =
+          htons(peer_port) // Socket Internet Port htons = Host to Network Short
   };
+
+  /*
+    inet_pton (Internet Presentation to Network)
+    standard C library function used to convert human-readable IP addresses
+    (like "192.168.1.1" or "2001:db8::1") into their binary network byte-order
+    representation
+  */
+  if (inet_pton(AF_INET, peer_ip, &(peer_adr.sin_addr)) <= 0) {
+    perror("ERROR: Something wrong with IP address");
+    return EXIT_FAILURE;
+  }
+
+  /* SOCK_DGRAM = fast & connectionless, just sending to an address & not
+   * establishing a connection prior */
+  int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (udp_socket <= 0) {
+    perror("ERROR: Failed to create socket");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
